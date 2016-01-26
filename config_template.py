7173aafea3271d3545b2cfa648326cb9dc5b27c2
shenfei <shenfei361@gmail.com>
2016-01-26 18:04:27 +0800

    add config_template for illustration

---
+++ config_template.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+TRELLO_CONFIG = {
+    'api_key':'your_api_key',
+    'api_secret':'your_api_secret',
+    'oauth_token':'your_token',
+    'oauth_token_secret':'your_token_secret'
+}
+
+BOARD_ID = 'your_board_id'
+
+DEV_MEMBERS = {
+    'member_id': 'member_name',
+}
+
+DOING_LISTS = ['doing_list_id']
+
+DONE_LISTS = ['done_list_id']
+
+bearychat_url = 'https://your_hook_url'
+bearychat_channels = ['your_channel_name']
